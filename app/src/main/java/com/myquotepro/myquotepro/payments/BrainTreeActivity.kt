7e2ff6dfd80c3f68ebbaaaf33414ccb388115611
manxoloh <maithyakavyu@outlook.com>
2018-12-19 15:03:03 +0300

    Updates

---
+++ app/src/main/java/com/myquotepro/myquotepro/payments/BrainTreeActivity.kt
@@ -0,0 +1,13 @@
+package com.myquotepro.myquotepro.payments
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.myquotepro.myquotepro.R
+
+class BrainTreeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brain_tree)
+    }
+}
